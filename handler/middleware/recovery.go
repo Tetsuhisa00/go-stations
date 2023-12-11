28d5bc0737d52717831f1fdfa932139ce376a65a
Tetsuhisa00 <tetsuhisadevelop@gmail.com>
2023-12-11 16:46:21 +0900

    fix format and erase unnecessary comments

--- handler/middleware/recovery.go
+++ handler/middleware/recovery.go
@@ -6,17 +6,14 @@ import (
 )
 
 func Recovery(h http.Handler) http.Handler {
-fn := func(w http.ResponseWriter, r *http.Request) {
-// TODO: ここに実装をする
-
-	// 前処理をここに実装する
-    defer func() {
-		if err := recover(); err != nil {
-			log.Println(err)
-			w.WriteHeader(http.StatusInternalServerError)
-		}
-	}()
-	h.ServeHTTP(w, r)
-}
-return http.HandlerFunc(fn)
+    fn := func(w http.ResponseWriter, r *http.Request) {
+        defer func() {
+            if err := recover(); err != nil {
+                log.Println(err)
+                w.WriteHeader(http.StatusInternalServerError)
+            }
+        }()
+        h.ServeHTTP(w, r)
+    }
+    return http.HandlerFunc(fn)
 }
