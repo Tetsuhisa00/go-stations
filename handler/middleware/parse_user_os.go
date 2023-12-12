d6d94753069e29c26b51f193785d72840eabd48d
Tetsuhisa00 <tetsuhisadevelop@gmail.com>
2023-12-13 02:58:48 +0900

    Add request information logging in ParseUserOS middleware

--- handler/middleware/parse_user_os.go
+++ handler/middleware/parse_user_os.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"net/http"
 	"time"
+	"fmt"
 
 	"github.com/mileusna/useragent"
 )
@@ -19,10 +20,26 @@ type RequestInfo struct {
 }
 
 func ParseUserOS(h http.Handler) http.Handler {
+
 	fn := func(w http.ResponseWriter, r *http.Request) {
+
+		start := time.Now()
+		
 		ua := useragent.Parse(r.UserAgent())
 		ctx := context.WithValue(r.Context(), uoskey, ua.OS)
 		h.ServeHTTP(w, r.WithContext(ctx))
+
+		finish := time.Now()
+		latency := finish.Sub(start).Milliseconds()
+
+		reqInfo := &RequestInfo {
+			Timestamp: finish,
+			Latency: latency,
+			Path: r.URL.Path,
+			OS: ua.OS,
+		}
+		fmt.Println(reqInfo)
 	}
+
 	return http.HandlerFunc(fn)
 }
