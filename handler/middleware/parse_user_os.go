5e3b5b7403055d7815f43a9b2af48b6c8a1cfd04
Tetsuhisa00 <tetsuhisadevelop@gmail.com>
2023-12-13 02:46:04 +0900

    Add RequestInfo struct to store request information

--- handler/middleware/parse_user_os.go
+++ handler/middleware/parse_user_os.go
@@ -3,11 +3,19 @@ package middleware
 import (
 	"context"
 	"net/http"
+	"time"
+
 	"github.com/mileusna/useragent"
 )
 
 type userOSKey string 
 const uoskey = userOSKey("UserOS")
+
+type RequestInfo struct {
+	Timestamp time.Time
+	Latency int64
+	OS string
+}
 
 func ParseUserOS(h http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
