718e0ee3ea3dc787e91ff57328d5d5bb343650a2
Tetsuhisa00 <tetsuhisadevelop@gmail.com>
2023-12-13 22:22:12 +0900

    Refactor UserOSKey type and constant name
    This commit refactors the `UserOSKey` type to `userOSKey` and renames the constant to `uoskey`. This change improves code readability and follows naming conventions.

--- handler/middleware/parse_user_os.go
+++ handler/middleware/parse_user_os.go
@@ -6,12 +6,13 @@ import (
 	"github.com/mileusna/useragent"
 )
 
-type UserOSKey string
+type userOSKey string 
+const uoskey = userOSKey("UserOS")
 
 func ParseUserOS(h http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
 		ua := useragent.Parse(r.UserAgent())
-		ctx := context.WithValue(r.Context(), UserOSKey("UserOS"), ua.OS)
+		ctx := context.WithValue(r.Context(), uoskey, ua.OS)
 		h.ServeHTTP(w, r.WithContext(ctx))
 	}
 	return http.HandlerFunc(fn)
