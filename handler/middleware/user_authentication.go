0d60e637fdc6a614ecebc4c0fe29bb1613bd1121
Tetsuhisa00 <tetsuhisadevelop@gmail.com>
2023-12-14 20:21:43 +0900

    Add Basic authentication and error logging in UserAuthentication middleware

--- handler/middleware/user_authentication.go
+++ handler/middleware/user_authentication.go
@@ -3,19 +3,24 @@ package middleware
 import (
 	"net/http"
 	"os"
+	"log"
 )
 
 func UserAuthentication(h http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("WWW-Authenticate", `Basic realm="Enter username and password"`)
 		username, password, ok := r.BasicAuth()
-		if !ok {
+		if !ok || username == "" || password == "" {
 			w.WriteHeader(http.StatusUnauthorized)
+			log.Println("Basic認証エラー: 認証情報が提供されていないです。")
 			return
 		}
+		
 		if username == os.Getenv("BASIC_AUTH_USER_ID") && password == os.Getenv("BASIC_AUTH_PASSWORD") {
 			h.ServeHTTP(w, r)
 		} else {
 			w.WriteHeader(http.StatusUnauthorized)
+			log.Println("Basic認証エラー: ユーザーIDまたはパスワードが間違っています。")
 			return 
 		}
 	}
