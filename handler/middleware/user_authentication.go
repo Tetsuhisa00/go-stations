29b954556090ec5ef78ccb88ec6a2883c26d7736
Tetsuhisa00 <tetsuhisadevelop@gmail.com>
2023-12-15 22:30:37 +0900

    Refactor user authentication middleware to use 'userid' instead of 'username'

--- handler/middleware/user_authentication.go
+++ handler/middleware/user_authentication.go
@@ -7,17 +7,16 @@ import (
 
 func UserAuthentication(h http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
-		username, password, ok := r.BasicAuth()
+		userid, password, ok := r.BasicAuth()
 		if !ok {
 			w.WriteHeader(http.StatusUnauthorized)
 			return
 		}
-		if username == os.Getenv("BASIC_AUTH_USER_ID") && password == os.Getenv("BASIC_AUTH_PASSWORD") {
-			h.ServeHTTP(w, r)
-		} else {
+		if userid != os.Getenv("BASIC_AUTH_USER_ID") || password != os.Getenv("BASIC_AUTH_PASSWORD") {
 			w.WriteHeader(http.StatusUnauthorized)
-			return 
+			return
 		}
+		h.ServeHTTP(w, r)
 	}
 	return http.HandlerFunc(fn)
 }
