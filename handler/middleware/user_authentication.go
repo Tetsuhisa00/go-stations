38645073a90463e6cb470d92944939a9374cc8ec
Tetsuhisa00 <tetsuhisadevelop@gmail.com>
2023-12-15 22:25:55 +0900

    Update user authentication middleware to use 'userid' instead of 'username' in Basic Auth

--- handler/middleware/user_authentication.go
+++ handler/middleware/user_authentication.go
@@ -8,15 +8,15 @@ import (
 
 func UserAuthentication(h http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("WWW-Authenticate", `Basic realm="Enter username and password"`)
-		username, password, ok := r.BasicAuth()
-		if !ok || username == "" || password == "" {
+		w.Header().Set("WWW-Authenticate", `Basic realm="Enter userid and password"`)
+		userid, password, ok := r.BasicAuth()
+		if !ok || userid == "" || password == "" {
 			w.WriteHeader(http.StatusUnauthorized)
 			log.Println("Basic認証エラー: 認証情報が提供されていないです。")
 			return
 		}
 
-		if username == os.Getenv("BASIC_AUTH_USER_ID") && password == os.Getenv("BASIC_AUTH_PASSWORD") {
+		if userid == os.Getenv("BASIC_AUTH_USER_ID") && password == os.Getenv("BASIC_AUTH_PASSWORD") {
 			h.ServeHTTP(w, r)
 		} else {
 			w.WriteHeader(http.StatusUnauthorized)
