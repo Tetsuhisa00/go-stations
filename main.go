01067ff3e582d730e18a56e38bd4984eb26d5bad
Tetsuhisa00 <tetsuhisadevelop@gmail.com>
2023-12-25 21:53:23 +0900

    Add graceful shutdown to server

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"time"
+	"fmt"
+
 	//"github.com/TechBowl-japan/go-stations/handler"
 
 	"github.com/TechBowl-japan/go-stations/db"
@@ -57,5 +61,29 @@ func realMain() error {
 	log.Println("server start at port 8080")
 	log.Fatal(http.ListenAndServe(":8080", mux))
 
+	srv := &http.Server{
+		Addr:    port,
+		Handler: mux,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+            log.Fatal(err)
+        }
+    }()
+	
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill)
+	defer stop()
+
+	<-ctx.Done()
+
+	ctxShutDown, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctxShutDown); err != nil {
+		log.Fatal(err)
+		return err
+	}
+	fmt.Println("Server exited successfully!!")
 	return nil
 }
