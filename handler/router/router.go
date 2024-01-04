2e059a8b58725f99a3a7366d2bd3ce6f3655b18f
Tetsuhisa00 <tetsuhisadevelop@gmail.com>
2024-01-04 18:37:04 +0900

    Add graceful shutdown handler to router

--- handler/router/router.go
+++ handler/router/router.go
@@ -15,10 +15,12 @@ func NewRouter(todoDB *sql.DB) *http.ServeMux {
 	todoService := service.NewTODOService(todoDB)
 	todoHandler := handler.NewTODOHandler(todoService)
 	panicHandler := handler.NewPanicHandler()
+	gracefulHandler := handler.NewGracefulHandler()
 
 	mux := http.NewServeMux()
 	mux.Handle("/healthz", middleware.UserAuthentication(&handler.HealthzHandler{}))
 	mux.Handle("/todos", todoHandler)
 	mux.Handle("/do-panic", middleware.Recovery(panicHandler))
+	mux.Handle("/graceful", gracefulHandler)
 	return mux
 }
